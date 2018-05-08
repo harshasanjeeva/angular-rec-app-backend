14a9a283c2544b95ecc06f38f46234e54e1a9944
suhasjanardhan <janardhansuhas@gmail.com>
2018-05-08 11:21:14 -0700

    adding DB constraints

--- dbm.js
+++ dbm.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
    username:{type:String, unique:true},
-   name:String,
-   email:String,
-   phone:String,
-   password:String,     
+   name:{type:String, required:true},
+   email:{type:String, required:true},
+   phone:{type:String, required:true},
+   password:{type:String, required:true},    
 });
 
 var User = mongoose.model("users",userSchema);
